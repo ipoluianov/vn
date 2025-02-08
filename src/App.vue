ec1bc04a90a197abfd485157837636930490fc7a
ip <ivan.poluianov@gmail.com>
2025-02-08 11:39:37 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,18 +13,9 @@ async function greet() {
 
 <template>
   <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
+    <h1>VN</h1>
 
     <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
     </div>
     <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
 
